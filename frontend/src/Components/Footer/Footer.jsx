30bd64060fb1f5909d70a38df8bb038a35543506
hamzaanees8 <hamza.anees.432@gmail.com>
2024-10-19 02:17:22 +0500

    updated version

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
-import footer_logo from '../Assets/logo_big.png'
-import instagram_icon from '../Assets/instagram_icon.png'
-import pintrest_icon from '../Assets/pintester_icon.png'
-import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import footer_logo from "../Assets/logo.jpeg";
+import instagram_icon from "../Assets/instagram_icon.png";
+import pintrest_icon from "../Assets/pintester_icon.png";
+import whatsapp_icon from "../Assets/whatsapp_icon.png";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
+        <p>KOOGLE ARDEN</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
@@ -22,13 +22,13 @@ const Footer = () => {
       </ul>
       <div className="footer-social-icons">
         <div className="footer-icons-container">
-            <img src={instagram_icon} alt="" />
+          <img src={instagram_icon} alt="" />
         </div>
         <div className="footer-icons-container">
-            <img src={pintrest_icon} alt="" />
+          <img src={pintrest_icon} alt="" />
         </div>
         <div className="footer-icons-container">
-            <img src={whatsapp_icon} alt="" />
+          <img src={whatsapp_icon} alt="" />
         </div>
       </div>
       <div className="footer-copyright">
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>Copyright @ 2023 - All Right Reserved.</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
